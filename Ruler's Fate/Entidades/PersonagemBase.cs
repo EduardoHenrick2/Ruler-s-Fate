4eb8171c7e7910c951b21bc64a72a26352ab6f33
Eduardo <eduardohenrick900@gmail.com>
2026-09-17 18:07:49 -0300

    acertanto o projeto

---
+++ Ruler's Fate/Entidades/PersonagemBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ruler_s_Fate.Entidades
+{
+
+    public abstract class PersonagemBase
+    {
+        public string Nome { get; protected set; }
+        public int VidaMax { get; protected set; }
+        public int VidaAtual { get; protected set; }
+        public int Mana { get; protected set; }
+        public int Forca { get; protected set; }
+        public int Inteligencia { get; protected set; }
+        public int Velocidade { get; protected set; }
+
+        public PersonagemBase(string nome, int vida, int mana, int forca, int inteligencia, int velocidade)
+        {
+            Nome = nome;
+            VidaMax = vida;
+            VidaAtual = vida;
+            Mana = mana;
+            Forca = forca;
+            Inteligencia = inteligencia;
+            Velocidade = velocidade;
+        }
+        public void ReceberDano(int dano)
+        {
+            if (VidaAtual < 0 )
+            {
+                VidaAtual = 0;
+            }
+            Console.WriteLine($"{Nome} recebeu {dano} de dano! Vida restante: {VidaAtual}/{VidaMax}");
+        }
+
+    }
+}
