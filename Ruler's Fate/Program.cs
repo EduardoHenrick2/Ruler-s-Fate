48b2112e4e66b0621aa5244f3f4b5dc0dfa7a73b
Eduardo <eduardohenrick900@gmail.com>
2026-09-17 17:52:06 -0300

    Adicionar arquivos de projeto.

---
+++ Ruler's Fate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ruler_s_Fate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
